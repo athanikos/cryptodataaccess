8dd3e8820bd7f4d577a64a34b981b8aa6c6a7e90
nikos <athanikos@gmail.com>
2020-08-26 15:02:17 +0300

    renames user_id get transactions by user_id before date order_type transaction_type check_every start_date end_date CalculatorRepository.py

--- build/lib/cryptodataaccess/Calculator/CalculatorRepository.py
+++ build/lib/cryptodataaccess/Calculator/CalculatorRepository.py
@@ -1,34 +1,40 @@
-from datetime import datetime
-from cryptomodel.coinmarket import prices
-from cryptomodel.fixer import exchange_rates
-from cryptomodel.readonly import SymbolRates
-from mongoengine import Q
-from cryptodataaccess.helpers import server_time_out_wrapper, do_connect
+from cryptomodel.computed_notification import computed_notification
+from cryptomodel.operations import OPERATIONS
+
+from cryptodataaccess.Memory import Memory, CALCULATOR_MEMORY_KEY
+from cryptodataaccess.Repository import Repository
 
 DATE_FORMAT = "%Y-%m-%d"
 
 
-class CalculatorRepository:
+class CalculatorRepository(Repository):
 
-    def __init__(self, config, log_error):
-        self.configuration = config
-        self.log_error = log_error
+    def __init__(self, calculator_store):
+        self.calculator_store = calculator_store
+        self.computed_notifications = []
+        super(CalculatorRepository, self).__init__()
+        memory = Memory(on_add=self.calculator_store.insert_computed_notification,
+                        on_edit=None,
+                        on_remove=None,
+                        items=self.computed_notifications
+                        )
+        self.memories[CALCULATOR_MEMORY_KEY] = memory
 
-    def evaluate_variable(self):
-        pass
+    def add_computed_notification(self, user_id, user_name, user_email, notification_type, check_every, is_active,
+                         start_date, end_date, channel_type, threshold_value, source_id, computed_date, result):
 
-    def evaluate_expression(self):
-        pass
+        n = computed_notification(
+            user_id=user_id, user_name=user_name, user_email=user_email,
+            notification_type=notification_type,
+            check_every=check_every,
+            start_date=start_date,
+            end_date=end_date,
+            is_active=is_active,
+            channel_type=channel_type, threshold_value=threshold_value, source_id=source_id,
+            operation=OPERATIONS.ADDED.name, computed_date= computed_date, result=result)
 
+        self.memories[CALCULATOR_MEMORY_KEY].items.append(n)
+        return n
 
-class StatementExecutor:
-
-    def prepare(self):
-        pass
-
-    def execute(self):
-        pass
-
-
-class MongoEngineStatementExecutor(StatementExecutor):
-    pass
+    def get_computed_notification_before_date(self, user_id, date):
+        return self.calculator_store.fetch_computed_notifications_before_date(user_id, date)
