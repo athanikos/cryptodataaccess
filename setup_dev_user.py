0bcf12e70cd41721e4158587f3d886cc313b97f8
nikos <athanikos@gmail.com>
2021-02-22 19:16:00 +0200

    Local mongo and central mongo insert_symbols

--- setup_dev_user.py
+++ setup_dev_user.py
@@ -1,3 +1,8 @@
-from keyring import set_password
-set_password("cryptodataaccess", "USERNAME", "admin")
-set_password("cryptodataaccess", "admin", "admin")
+import keyring
+from keyrings.alt.file import PlaintextKeyring
+keyring.set_keyring(PlaintextKeyring())
+keyring.set_keyring(PlaintextKeyring())
+keyring.set_password('CryptoCalculatorService', 'LOCAL_MONGO_USERNAME', 'admin')
+keyring.set_password('CryptoCalculatorService', 'admin', 'admin')
+keyring.set_password('CryptoCalculatorService', 'CENTRAL_MONGO_USERNAME', 'admin')
+keyring.set_password('CryptoCalculatorService', 'admin', 'admin')
