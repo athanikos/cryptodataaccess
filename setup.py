2e9d7b09821e770c813e5e35a6a5dc68f7a3170d
nikos <athanikos@gmail.com>
2021-03-03 17:52:19 +0200

    Local mongo and central mongo insert_symbols

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='cryptodataaccess-athanikos',
-    version="0.6.1",
+    version="0.6.2",
     license='MIT License',
     author='Nikos Athanasakis',
     packages=setuptools.find_packages(),
